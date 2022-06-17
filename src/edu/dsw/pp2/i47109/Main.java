edc832f8ed64927e087cf95a189bc48e6d0aec53
Ada Witamborska <adrianna@tonik.com>
2022-06-17 12:41:31 +0200

    zad 3 - usuwanie delimiterow na spacje z danego pliku txt

--- src/edu/dsw/pp2/i47109/Main.java
+++ src/edu/dsw/pp2/i47109/Main.java
@@ -2,8 +2,12 @@ package edu.dsw.pp2.i47109;
 
 public class Main {
     static public void main(String args[]){
-        // zad 3
-        FileManager fileManager = new FileManager();
-        fileManager.copy();
+        // zad 2 - kopiowanie plikow
+        //FileManager fileManager = new FileManager();
+        //fileManager.copy();
+
+        // zad 3 zamiana delimetrow na spacje
+        FileParser fileParser = new FileParser();
+        fileParser.removeDelimeter();
     }
 }
