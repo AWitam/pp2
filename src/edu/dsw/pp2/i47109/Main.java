69d23c92fdf6fa2db37ea8cff5159e61967003a7
Ada Witamborska <adrianna@tonik.com>
2022-06-19 14:17:34 +0200

    add BigInt calculator solution

--- src/edu/dsw/pp2/i47109/Main.java
+++ src/edu/dsw/pp2/i47109/Main.java
@@ -2,12 +2,17 @@ package edu.dsw.pp2.i47109;
 
 public class Main {
     static public void main(String args[]) {
+        // zad 1 - dzialania na wielkich liczbach
+        BigIntCalculator bigIntCalculator = new BigIntCalculator();
+        bigIntCalculator.exec();
+
+
         // zad 2 - kopiowanie plikow
         FileCopier fileCopier = new FileCopier();
-        fileCopier.makeCopy();
+        // fileCopier.makeCopy();
 
         // zad 3 zamiana delimetrow na spacje
         FileParser fileParser = new FileParser();
-        fileParser.removeDelimiters();
+        // fileParser.removeDelimiters();
     }
 }
